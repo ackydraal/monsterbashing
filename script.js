85955e34aedf6115e698d48bec7fadb528b93da1
Tommy Björkman <tommy.bjorkman@live.se>
2022-10-06 10:55:05 +0200

    added 3 heroes and 1 monster

---
+++ script.js
@@ -0,0 +1,16 @@
+const Fenster {
+    health : 36,
+    strength : 12,
+};
+const Benthor {
+    health : 42,
+    strength : 31,
+};
+const Floury {
+    health : 41,
+    strength : 19,
+};
+const Foulbeast {
+    health : 77,
+    strength : 22,
+};
